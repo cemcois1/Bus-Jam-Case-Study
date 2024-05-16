73e97c27293682081b678afabe6a9b959f945257
cemal can yıldırım <56302638+cemcois1@users.noreply.github.com>
2024-05-16 19:44:38 +0300

    Seat Human Color implamented

--- Assets/Spesfic/Code/Bus System/Bus.cs
+++ Assets/Spesfic/Code/Bus System/Bus.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,11 +6,18 @@ namespace Spesfic.Code.Bus_System
 {
     public class Bus : MonoBehaviour
     {
+        public Action OnBussFull;
+
+        [SerializeField] private List<Seat> seats;
+        public bool allSeatsFull=> seats.TrueForAll(x => x.IsFull);        
         [SerializeField] private List<MeshRenderer> meshRenderers;
-                
+        [SerializeField] private Color busColor=Color.grey;
+        
         public void SetColor(Color color)
         {
             meshRenderers.ForEach(x => x.material.color = color);
+            busColor = color;
+            seats.ForEach(x=>x.SetColor(color));
         }
     }
 }
--- Assets/Spesfic/Code/Bus System/Seat.cs
+++ Assets/Spesfic/Code/Bus System/Seat.cs
@@ -4,6 +4,12 @@ namespace Spesfic.Code.Bus_System
 {
     public class Seat : MonoBehaviour
     {
-
+        public bool IsFull;
+        [SerializeField] private Transform human;
+        
+        public void SetColor(Color color)
+        {
+            human.GetComponent<MeshRenderer>().material.color = color;
+        }
     }
 }
